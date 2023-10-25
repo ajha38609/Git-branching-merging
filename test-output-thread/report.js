b74f92173e7b8259583981b89edd4721a4babd8e
AShutosh <ashutoshjha.072k@gmail.com>
2023-10-25 10:38:38 +0530

    done

---
+++ test-output-thread/report.js
@@ -0,0 +1,4 @@
+$(document).ready(function() {
+CucumberHTML.timelineItems.pushArray([{"id":"460790d9-9a68-4288-ae65-358b6750bbaf","feature":"Phase End project","scenario":"Star health home page test","start":1698149220749,"group":1,"content":"","tags":"@all,","end":1698149250538,"className":"passed"}]);
+CucumberHTML.timelineGroups.pushArray([{"id":1,"content":"Thread[#1,main,5,main]"}]);
+});
